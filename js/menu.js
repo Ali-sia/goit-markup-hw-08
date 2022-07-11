182c6390d330d375b2cdb79ad20ef03802158d4f
aliesia-zahorui <aliesia@ukr.net>
2022-07-11 12:35:27 +0300

    create header (partly)
    open/close mobile menu
    hide mobile toggle menu
    hide logo, when mobile menu is open

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,11 @@
 (() => {
   const menuBtnRef = document.querySelector('[data-menu-button]');
   const mobileMenuRef = document.querySelector('[data-menu]');
+  // mine code, work?
+  // const mobileMenuBlockRef = document.querySelector('[data-menu-mobile]');
+  const logoHideRef = document.querySelector('[data-logo-hide]');
+  // const desktopMenuRef = document.querySelector('[data-menu-desktop]');
+  // mine code, work?
 
   menuBtnRef.addEventListener('click', () => {
     const expanded = menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
@@ -9,5 +14,10 @@
     menuBtnRef.setAttribute('aria-expanded', !expanded);
 
     mobileMenuRef.classList.toggle('menu-container--is-open');
+    // mine code, work?
+    // mobileMenuBlockRef.classList.toggle('mobile-menu--is-open');
+    logoHideRef.classList.toggle('logo--is-hide');
+    // desktopMenuRef.classList.toggle('tablet-desktop-menu--is-open');
+    // mine code, work?
   });
 })();
