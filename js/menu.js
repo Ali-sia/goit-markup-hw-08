6093d0ff45eb1e0d572cba40bba41c1729385bcc
aliesia-zahorui <aliesia@ukr.net>
2022-07-20 20:41:51 +0300

    22

--- js/menu.js
+++ js/menu.js
@@ -2,6 +2,8 @@
   const menuBtnRef = document.querySelector('[data-menu-button]');
   const mobileMenuRef = document.querySelector('[data-menu]');
   // mine code, work?
+  const backdropMenuRef = document.querySelector('[data-menu-backdrop]');
+  // const buttonHideRef = document.querySelector('[data-button-hide]');
   // const mobileMenuBlockRef = document.querySelector('[data-menu-mobile]');
   const logoHideRef = document.querySelector('[data-logo-hide]');
   // const desktopMenuRef = document.querySelector('[data-menu-desktop]');
@@ -17,6 +19,8 @@
     // mine code, work?
     // mobileMenuBlockRef.classList.toggle('mobile-menu--is-open');
     logoHideRef.classList.toggle('logo--is-hide');
+    backdropMenuRef.classList.toggle('backdrop--is-hidden');
+    // buttonHideRef.classList.toggle('header__button--is-hidden');
     // desktopMenuRef.classList.toggle('tablet-desktop-menu--is-open');
     // mine code, work?
   });
